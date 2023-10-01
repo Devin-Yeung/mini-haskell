47d2eb7698f4dc1acec9260afb11b959b1104d29
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-10-01 23:35:57 +0800

    feat: tokenizer

--- crates/mini_haskell_lexer/src/lexer.rs
+++ crates/mini_haskell_lexer/src/lexer.rs
@@ -1,5 +1,5 @@
 use crate::error::LexingError;
-use logos::Logos;
+use logos::{Logos, SpannedIter};
 use mini_haskell_diagnostic::span::Span;
 
 #[derive(Logos, Debug, PartialEq)]
@@ -14,7 +14,7 @@ pub enum TokenTy {
     #[token("F", | _ | true)]
     BoolLit(bool),
 
-    #[regex(r"(?:[1-9][0-9]*|0)", | lex | lex.slice().parse::<usize>().unwrap())]
+    #[regex(r"(?:[1-9][0-9]*|0)", | lex | lex.slice().parse::< usize > ().unwrap())]
     // Nature number literal
     NatLit(usize),
 
@@ -72,16 +72,32 @@ impl Token {
     }
 }
 
+pub struct Tokenizer<'src> {
+    inner: SpannedIter<'src, TokenTy>,
+}
+
+impl<'src> Tokenizer<'src> {
+    pub fn new<S: AsRef<str> + ?Sized>(source: &'src S) -> Tokenizer<'src> {
+        Tokenizer {
+            inner: TokenTy::lexer(source.as_ref()).spanned(),
+        }
+    }
+}
+
+impl<'src> Iterator for Tokenizer<'src> {
+    type Item = Result<Token, LexingError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|(token, span)| match token {
+            Ok(ty) => Ok(Token::new(ty, span.into())),
+            Err(_) => Err(LexingError::UnexpectedToken(span.into())),
+        })
+    }
+}
+
 impl Token {
     pub fn tokens<S: AsRef<str>>(source: &S) -> Vec<Result<Token, LexingError>> {
-        let lexer = TokenTy::lexer(source.as_ref());
-        lexer
-            .spanned()
-            .map(|(token, span)| match token {
-                Ok(ty) => Ok(Token::new(ty, span.into())),
-                Err(_) => Err(LexingError::UnexpectedToken(span.into())),
-            })
-            .collect::<Vec<_>>()
+        Tokenizer::new(source.as_ref()).collect::<Vec<_>>()
     }
 }
 
